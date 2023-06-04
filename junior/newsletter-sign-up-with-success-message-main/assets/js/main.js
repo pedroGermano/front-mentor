4ba71e8944f2df2c8afae478c4bc7474b5d51757
Pedro Germano <pedrogermano232@gmail.com>
2023-06-04 11:29:52 -0300

    Done

---
+++ junior/newsletter-sign-up-with-success-message-main/assets/js/main.js
@@ -0,0 +1,5 @@
+const containerEl = document.getElementsByClassName('container')
+const textContainer = document.getElementsByClassName('container__text')
+const imgContainer = document.getElementsByClassName('container__img')
+
+
